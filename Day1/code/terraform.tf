882b98470264f00734bd45b327a40453f761a037
techwithburhan <techdeployers.com@gmail.com>
2026-04-02 01:54:19 +0530

    add comments

--- Day1/code/terraform.tf
+++ Day1/code/terraform.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "6.38.0"
+      source  = "hashicorp/aws"   # tells Terraform to use AWS provider from HashiCorp
+      version = "6.38.0"          # fixes the provider version (so no unexpected changes)
     }
   }
 }
 
 provider "aws" {
-  region = eu-west-1
+  region = "eu-west-1"            # AWS region where resources will be created (Ireland)
 }
